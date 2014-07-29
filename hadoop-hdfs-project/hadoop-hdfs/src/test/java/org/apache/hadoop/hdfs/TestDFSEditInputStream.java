ef4c2f5b5082ddc4e4621705afab100733d2a08f
James Thomas <james.thomas@cloudera.com>
2014-07-29 14:09:11 -0700

    Temp TestDFSEditInputStream work

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestDFSEditInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestDFSEditInputStream.java
@@ -1,6 +1,7 @@
 package org.apache.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.server.namenode.FSEditLogOp;
@@ -11,6 +12,14 @@ import java.io.IOException;
 
 public class TestDFSEditInputStream {
 
+  private static final Configuration conf = new Configuration();
+
+  static {
+    // Don't retry connections - it just slows down the tests.
+    conf.setInt(CommonConfigurationKeysPublic.IPC_CLIENT_CONNECT_MAX_RETRIES_KEY, 0);
+  }
+
+  // make this an end-to-end test, throw the JNs in there too
   @Test
   public void testBasic() throws IOException {
     Configuration conf = new HdfsConfiguration();
@@ -23,6 +32,14 @@ public class TestDFSEditInputStream {
     client.rename("/file", "/file2", null);
     FSEditLogOp next = null;
     while ((next = eis.next()) == null);
+    // force a null or two
+    eis.next();
+    eis.next();
     System.out.println(next);
+  }
+
+  @Test
+  public void testValidEditsOnly() {
+    // mock the ClientProtocol and pass it to DFSEditInputStream
   }
 }
